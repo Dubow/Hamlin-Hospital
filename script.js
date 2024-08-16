81b03c2119d8e90f8961557a3a160b38d7225aa8
Abdirahman <abdirahmandubow4@gmail.com>
2024-08-16 16:43:20 +0800

    updating changes

--- script.js
+++ script.js
@@ -20,9 +20,6 @@ app.get('/about', (req, res) => {
     res.sendFile(__dirname + '/public/Hamlin/about.html');
 });
 
-app.get('/home', (req, res) => {
-    res.sendFile(__dirname + '/public/Hamlin/index.html');
-});
 
 app.post('/', (req, res) => {
     console.log(req.body);
